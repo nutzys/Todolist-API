b87ef1efa778e82f832ca38226b1cc23851ea1ab
Ulvis Dulbergs <ulvisdulbergs@Ulviss-MacBook-Air.local>
2023-03-09 13:36:43 +0200

    finished v1

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   
   const [todo, setTodo] = useState<Todo[]>([]);
   const [id, setId] = useState<number>(0);
-  const [page, setPage] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
   const [limit, setLimit] = useState<number>(200);
   const [user, setUser] = useState<Todo[]>([]);
   const [isUser, setIsUser] = useState<boolean>(false);
@@ -23,34 +23,11 @@ function App() {
   const onSaveId = (uid: number) => {
     setId(uid);
   }
-
-  const onClickPage = (page: number) => {
-    setPage(page);
-  }
-  const {data: todoQuery, isLoading} = useQuery({
-    queryKey: ['todos', page, limit],
-    queryFn: () => fetchTodos(page, limit),
-    keepPreviousData: true,
-    onSuccess: data => {
-      setIsUser(false);
-      setTodo(data);
-    }
-  })  
-  
-  const {data: userQuery} = useQuery({
-    queryKey: ['users', id, 'todos'],
-    queryFn: () => fetchUsers(id),
-    onSuccess: data => {
-      setIsUser(true);
-      setUser(data);
-    }
-  })
   
   return (
     <div className="App">
       <TodoForm onSaveCount={onSaveCount} onSaveId={onSaveId}/>
-      {!isUser && <TodoList todo={todo} page={page} onClickPage={onClickPage} isLoading={isLoading}/>}
-      {isUser && <UserList user={user}/>}
+      <TodoList todo={todo} limit={limit} id={id}/>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,42 +1,69 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react'
+import fetchTodos from '../api/fetchTodos';
+import fetchUsers from '../api/fetchUsers';
 import { Todo } from '../model/todo';
 import TodoItem from './TodoItem'
 import './TodoList.css';
 
 interface Props {
   todo: Todo[],
-  page: number,
-  onClickPage: (page: number) => void,
-  isLoading: boolean
+  limit: number,
+  id: number,
 }
 
 const TodoList: React.FC<Props> = (props) => {
   
-  const [page, setPage] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
+  const [todo, setTodo] = useState<Todo[]>([]);
+  const [user, setUser] = useState<Todo[]>([]);
+  const [isUser, setIsUser] = useState<boolean>(false);
 
+  const {data: todoQuery, isLoading} = useQuery({
+    queryKey: ['todos', page, props.limit],
+    queryFn: () => fetchTodos(page, props.limit),
+    keepPreviousData: true,
+    onSuccess: data => {
+      setIsUser(false);
+      setTodo(data);
+    }
+  })  
+  const {data: userQuery} = useQuery({
+    queryKey: ['users', props.id, 'todos'],
+    queryFn: () => fetchUsers(props.id),
+    onSuccess: data => {
+      setIsUser(true);
+      setUser(data);
+    }
+  })
   const clickPlusHandler = () => {
     setPage(old => old + 1);
-    props.onClickPage(page);
   }
-
+  
   const clickMinusHandler = () => {
     setPage(old => old - 1);
-    props.onClickPage(page);
   }
 
-  if(props.isLoading) return (<h1>Loading...</h1>)
+
+  if(isLoading) return (<h1>Loading...</h1>)
 
   return (
     <div>
-      {props.todo?.map((item: Todo) => (
-      <TodoItem
-      title={item.title}
-      userId={item.userId}
-      key={item.id}
-      />
-      ))}
+      {!isUser ? todo.map(item => (
+        <TodoItem
+        title={item.title}
+        userId={item.userId}
+        />
+      )) : (
+        user.map(user => (
+          <TodoItem 
+          title={user.title}
+          userId={user.userId}
+          />
+        ))
+      )}
       <div>
-        <button onClick={(e) => clickMinusHandler()} disabled={page === 0} className="btn">Previous</button>
+        <button onClick={(e) => clickMinusHandler()} disabled={page === 1} className="btn">Previous</button>
         Page: {page}
         <button onClick={(e) => clickPlusHandler()} className="btn">Next</button>
       </div>
